2c60740fdb6175ccac3f6bc173976008dfa0b59f
cortelucas <corte1994.lc@gmail.com>
2021-01-20 15:39:24 -0300

    - implementação do fluxo de usuário

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,8 +4,16 @@ const User = require('../models/User');
 const router = express.Router();
 
 router.post('/register', async(req, res) => {
+    const { email } = req.body;
+
     try {
+        if (await User.findOne({ email })) {
+            return res.status(400).send({ error: 'Usuário existente.' });
+        }
+
         const user = await User.create(req.body);
+
+        user.password = undefined;
 
         return res.send({ user });
     } catch (err) {
