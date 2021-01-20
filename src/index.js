25e03008904daf81fa53918aab1e4407c2fe23c3
cortelucas <corte1994.lc@gmail.com>
2021-01-20 16:42:50 -0300

    - reestruturação das pastas; - criação de um index de controllers que importa todos controllers;

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require('./controllers/authController')(app);
-require('./controllers/projectController')(app);
+require('./app/controllers/index')(app);
 
 const port = 8000;
 app.listen(port, () => console.log(`Servidor rodando em 'http://localhost:${port}'`))
